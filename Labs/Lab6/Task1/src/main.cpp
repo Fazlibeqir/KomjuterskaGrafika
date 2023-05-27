1617e354fd911a03ad4d6053e6b494776bfdbf84
Beqir Fazli <57021385+Fazlibeqir@users.noreply.github.com>
2023-05-27 21:38:00 +0200

    MainLooksNice

--- Labs/Lab6/Task1/src/main.cpp
+++ Labs/Lab6/Task1/src/main.cpp
@@ -160,19 +160,18 @@ int main() {
 
     // render boxes
     glBindVertexArray(VAO);
-    for (unsigned int i = 0; i < 10; i++) {
       // calculate the model matrix for each object and pass it to shader before
       // drawing
       glm::mat4 model = glm::mat4(
           1.0f); // make sure to initialize matrix to identity matrix first
       model = glm::translate(model, cubePositions);
-      float angle = 20.0f * i;
+      float angle = 20.0f ;
       model =
           glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
       ourShader.setMat4("model", model);
 
       glDrawArrays(GL_TRIANGLES, 0, 36);
-    }
+
 
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
     // etc.)
